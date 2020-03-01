ee311230c36cdea77e32687218fab82bbfa52d11
Rekha Ballur <rnmadalli@gmail.com>
2020-02-29 23:32:52 -0500

    Jaws DB changes

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "REKHA",
-  database: "burgers_db"
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "REKHA",
+    database: "burgers_db"
 });
 
 // Make connection.
